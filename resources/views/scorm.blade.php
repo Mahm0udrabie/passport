954da553ca726ab220fcb93109eac45a6b4546fb
mahmoudrabie <m.ahmedmedo457@gmail.com>
2022-03-04 01:23:27 +0200

    make it better

--- resources/views/scorm.blade.php
+++ resources/views/scorm.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">Upload Scorm</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('store-scorm') }}" enctype="multipart/form-data">
