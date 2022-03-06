93b4086214933814bde95e0550a82eed6c9b0969
mahmoudrabie <m.ahmedmedo457@gmail.com>
2022-03-06 11:27:48 +0200

    test scorm

--- resources/views/scorm-view-test.blade.php
+++ resources/views/scorm-view-test.blade.php
@@ -3,5 +3,6 @@
 {{-- <iframe src="https://mahm0udrabie.github.io/scorm/story.html" width="100%" height="100%" frameborder="0"></iframe> --}}
 
 
+
 <iframe src="https://mahm0udrabie.github.io/scorm/story.html" height="100%" width="100%" title="Iframe Example"></iframe>
 
